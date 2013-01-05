8250dc4b4e279b331ca8095eca9fc722cf54099a
Maxim Andreev <andreevmaxim@gmail.com>
2013-01-05 13:33:37 +0400

    Fix default theme path in example config to work with linux (old path was for FreeBSD)

--- example.rc.lua
+++ example.rc.lua
@@ -41,7 +41,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init("/usr/local/share/awesome/themes/default/theme.lua")
+beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "xterm"
